8d39d294650a599d82db0c7789e8afc2b39982e2
grrnhsslr <hasslergarren@gmail.com>
2024-03-12 18:18:21 -0500

    made it regex

--- exercise1.py
+++ exercise1.py
@@ -1,6 +1,11 @@
+import re
+
 # Write a function that takes in a tweet and returns a list of the hashtags in the tweet
+def extract_hashtags(tweet):
+    hashtags = re.findall(r'#\w+', tweet)
+    return hashtags
+
+
 tweet = 'I love deep fried peanut butter!!! #peanut #butter #winner #winner #chicken #dinner'
-
-winner = [i for i in tweet.split() if i.startswith("#")]
-
-print(winner)
+hashtags = extract_hashtags(tweet)
+print(hashtags)
